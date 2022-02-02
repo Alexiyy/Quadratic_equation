1ddf6e4dc064f28a80979cfc23728f3d32e19d5c
Андрей Мухин <andreymuhin02@gmail.com>
2022-02-02 08:05:56 +0300

    x2

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -13,21 +13,10 @@ def x2(x):
     return collatz(x)
 
 def x3_1(x):
-    x = x * 3 + 1
-    sp.append(int(x))
-    return collatz(x)
+   
 
 def collatz(x):
-    if x in sp:
-        print ('Значение уже есть в списке')
-        return input_valid()
-    while x != 1:
-        sp.append(x)
-        return x
-    elif x % 2 == 0:
-        return x2(x)
-    else:
-        return x3_1(x)
+
 
 sp = []
 input_valid()
