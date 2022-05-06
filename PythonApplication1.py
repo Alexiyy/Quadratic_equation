886a56f3c305a3c2fb2f02c5e6052af90049f14e
Даниил <kourin.daniil@gmail.com>
2022-05-06 14:20:24 +0300

    dev

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,17 +1,30 @@
-import math as m
+def input_valid():
+    while True:
+        x = int(input("Р’РІРµРґРёС‚Рµ РїРµСЂРµРјРµРЅРЅСѓСЋ x = "))
+        if x > 0:
+            sp.append(x)
+            collatz(x)
 
+            break
+        print("РќРµРІРµСЂРЅРѕРµ С‡РёСЃР»Рѕ, РїРѕРїСЂРѕР±СѓР№С‚Рµ РґСЂСѓРіРѕРµ")
+    return x
 
-a = int(input ('Введи коэффицент a:'))
-b = int(input ('Введи коэффицент b:'))
-c = int(input ('Введи коэффицент c:'))
+def x2(x):
+    x = x / 2
+    sp.append(int(x))
+    return collatz(x)
 
-d =b*b - 4*a*c
+def x3_1(x):
+    x = x * 3 + 1
+    sp.append(int(x))
+    return collatz(x)
 
-if(d > 0):
-    x1 = (-b + m.sqrt(d))/(2*a)
-    x2 = (-b - m.sqrt(d))/(2*a)
-    print('Корни уравнения равны ' + str(x1) + ' и ' + str(x2) )
-elif(d == 0):
-    x = -b/(2*a)
-    print('Корень уравнения равен ' + str(x))
-else: print('Уравнение не имеет корней')
+def collatz(x):
+    while x != 1:
+        if x % 2 == 0:
+            return x2(x)
+        else:
+            return x3_1(x)
+    print('РЎРїРёСЃРѕРє РёРјРµРµС‚ РІРёРґ: ', sp)
+sp = []
+input_valid()
