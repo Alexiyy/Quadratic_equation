43ed8ceda18a8ade0642f43f5e66fcd17244aa36
Kirleoke <98177539+Kirleoke@users.noreply.github.com>
2022-05-16 16:36:32 +0300

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,11 +1,4 @@
 def input_valid():
-    while True:
-        x = int(input("Введите переменную x = "))
-        if x > 0:
-            collatz(x)
-            break
-        print("Неверное число, попробуйте другое")
-    return x
 
 def x2():
     pass
